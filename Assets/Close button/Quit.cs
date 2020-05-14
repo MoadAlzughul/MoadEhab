84d979c223c2edbc61ce267f957cbcfb96648041
MoadAlzughul <41698539+MoadAlzughul@users.noreply.github.com>
2020-05-13 22:58:59 -0400

    Added Close Action

---
+++ Assets/Close button/Quit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void DoQuit()
+    {
+        Debug.Log("Quit Button Clicked");
+        Application.Quit();
+        
+    }
+
+  
+}
